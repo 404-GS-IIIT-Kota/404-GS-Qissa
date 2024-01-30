51fe66c8ff55e73765ce5298f2fc86c3c5f747c9
HiteshwarKaushik <thehiteshwarkaushik@gmail.com>
2024-01-30 21:48:10 +0530

    Added Sign Up page
    Added code for SignUp page
    Added /signup route
    changed react-icons on SignIn page to mui-icons
    Added SignUpImage

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Loader from "./component/Loader";
 import SignIn from './scenes/signin'
+import SignUp from "./scenes/signup";
 import { lazy, Suspense } from "react";
 
 const Home = lazy(() => import("./pages/Home"));
@@ -12,6 +13,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
         </Routes>
       </Suspense>
     </Router>
--- src/scenes/signin.jsx
+++ src/scenes/signin.jsx
@@ -1,31 +1,59 @@
-import React from 'react';
-import { FaFacebook, FaTwitter, FaGoogle, FaUser, FaLock } from 'react-icons/fa';
-import placeholderImage from '../../public/sign-up-image-1.jpg';
+import React from "react";
+import LockIcon from "@mui/icons-material/Lock";
+import PersonIcon from "@mui/icons-material/Person";
+import GoogleIcon from "@mui/icons-material/Google";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import placeholderImage from "/sign-up-image-1.jpg";
 
 const Signup = () => {
   return (
     <div className="h-screen flex items-center justify-center bg-gray-200">
-      <div className="w-80 h-80 bg-white mx-auto flex flex-col lg:flex-row" style={{ width: '85%', height: '85%' }}>
+      <div
+        className="w-80 h-80 bg-white mx-auto flex flex-col lg:flex-row rounded-2xl shadow-2xl"
+        style={{ width: "85%", height: "85%" }}
+      >
         {/* Left Section - Image */}
         <div className="w-full lg:w-1/2 p-6 flex flex-col items-center justify-center">
-          <img src={placeholderImage} alt="Placeholder" className="w-full h-full object-cover" style={{ width: '75%', height: '75%' }} />
-          <a href="#" className="text-gray-700 underline mt-4">Create a new account</a>
+          <img
+            src={placeholderImage}
+            alt="Placeholder"
+            className="w-full h-full object-cover"
+            style={{ width: "75%", height: "75%" }}
+          />
+          <a href="/signup" className="text-gray-700 hover:underline mt-4">
+            Create a new account
+          </a>
         </div>
 
         {/* Right Section - Signup Form */}
         <div className="w-full lg:w-1/2 p-6 bg-white rounded-lg flex flex-col justify-center">
-          <h2 className="text-5xl font-bold mb-4 text-center">Sign In</h2>
+          <h2 className="text-5xl font-bold mb-4 max-sm:text-center">
+            Sign In
+          </h2>
           <br />
           <br />
-          <form className="w-2/3 mx-auto"> {/* Centered the form elements */}
+          <form className="w-full md:w-full">
             <div className="mb-8 flex items-center">
-              <FaUser className="text-black-800 mr-2" />
-              <input type="text" id="name" name="name" placeholder="Your Name" className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500" />
+              <PersonIcon className="text-black-800 mr-2" />
+              <input
+                type="text"
+                id="name"
+                name="name"
+                placeholder="Username"
+                className="w-3/5 px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+              />
             </div>
 
             <div className="mb-8 flex items-center">
-              <FaLock className="text-black-800 mr-2" />
-              <input type="password" id="password" name="password" placeholder='Password' className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500" />
+              <LockIcon className="text-black-800 mr-2" />
+              <input
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Password"
+                className="w-3/5 px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+              />
             </div>
 
             <div className="mb-6">
@@ -36,20 +64,25 @@ const Signup = () => {
               </label>
             </div>
 
-            <button type="submit" className="w-2/5 bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 mx-auto">Log In</button>
+            <button
+              type="submit"
+              className="w-2/5 bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 flex max-sm:items-center justify-center"
+            >
+              Log In
+            </button>
           </form>
           <br />
           <br />
-          <div className="mt-4 flex items-center justify-center">
+          <div className="mt-4 flex items-center">
             <span className="text-gray-700 text-sm">Or login with </span>
-            <FaFacebook className="text-blue-600 ml-2 cursor-pointer" />
-            <FaTwitter className="text-blue-400 ml-2 cursor-pointer" />
-            <FaGoogle className="text-red-500 ml-2 cursor-pointer" />
+            <FacebookIcon className="text-blue-600 ml-2 cursor-pointer" />
+            <TwitterIcon className="text-blue-400 ml-2 cursor-pointer" />
+            <GoogleIcon className="text-red-500 ml-2 cursor-pointer" />
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Signup;
