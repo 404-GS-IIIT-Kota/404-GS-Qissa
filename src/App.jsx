4236ac861dbe1c2def510a1cf1cfd939570cce6b
DhairyaMajmudar <2022kuec2045@iiitkota.ac.in>
2024-02-01 19:47:49 +0530

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Loader from "./component/Loader";
-import SignIn from './pages/signin'
-import SignUp from "./pages/signup";
+import SignIn from "./pages/Signin";
+import SignUp from "./pages/Signup";
+import Temp from "./pages/Temp";
 import { lazy, Suspense } from "react";
 
 const Home = lazy(() => import("./pages/Home"));
@@ -14,6 +15,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/signin" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
+          <Route path="/temp" element={<Temp />} />
         </Routes>
       </Suspense>
     </Router>
