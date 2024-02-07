4ec003bc57e8e901a603e722b92e7266a1635b39
DhairyaMajmudar <2022kuec2045@iiitkota.ac.in>
2024-02-07 22:56:03 +0530

    vg

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  routes: [
-    {
-      src: '/signin',
-      dest: '/404-gs-qissa.vercel.app/signin'
-    },
-    {
-      src: '/signup',
-      dest: '/404-gs-qissa.vercel.app/signup'
-    },
-  ],
 })
-
-
-
