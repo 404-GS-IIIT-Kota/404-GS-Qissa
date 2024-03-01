7184d97e6926147a0c9f9667601ca21051fcb4f1
Dhairya Majmudar <2022kuec2045@iiitkota.ac.in>
2024-03-02 02:34:57 +0530

    test pr

--- client/src/component/AddComment.jsx
+++ client/src/component/AddComment.jsx
@@ -41,7 +41,7 @@ const AddComment = ({ postId }) => {
             <svg
               className="w-5 h-5"
               aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
+              xmlns="http://www.w3.org/2000/svg"    
               fill="none"    
               viewBox="0 0 20 20"
             >
